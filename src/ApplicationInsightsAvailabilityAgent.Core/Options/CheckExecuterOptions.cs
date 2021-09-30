d3e09b54af431340ac90e523723563e7fa39e079
Sternheim, Sascha <uia60997@contiwan.com>
2021-09-30 20:06:03 +0200

    Set default value for CheckInterval

--- src/ApplicationInsightsAvailabilityAgent.Core/Options/CheckExecuterOptions.cs
+++ src/ApplicationInsightsAvailabilityAgent.Core/Options/CheckExecuterOptions.cs
@@ -8,6 +8,11 @@ namespace ApplicationInsightsAvailabilityAgent.Core.Options
         public string RunLocation { get; set; }
         public TimeSpan CheckInterval { get; set; }
 
+        public CheckExecuterOptions()
+        {
+            CheckInterval = TimeSpan.FromMinutes(5);
+        }
+
 #pragma warning disable CA2227 // It is configuration DTO
         public Dictionary<string, AvailabilityCheckOptions> Checks { get; set; }
 #pragma warning restore CA2227 // Sammlungseigenschaften müssen schreibgeschützt sein
